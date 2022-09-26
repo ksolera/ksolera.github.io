6888fb9475bd208b08816f10f721be09fe0fc737
ksolera <79610570+ksolera@users.noreply.github.com>
2022-09-26 18:54:53 -0400

    Create default.js

---
+++ itis3135/scripts/default.js
@@ -0,0 +1,6 @@
+const onLoad = () => {
+    document.getElementById("validation_link_html").setAttribute("href", "https://validator.w3.org/check?uri=" + location.href);
+    document.getElementById("validation_link_css").setAttribute("href", "https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);
+}
+window.addEventListener('DOMContentLoaded' , onLoad)
+
